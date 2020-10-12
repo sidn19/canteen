24f1d5e7eeaa1969ca02958340efb68ad94ee8e2
ArcTLK <adityanair313@gmail.com>
2020-10-13 00:21:15 +0530

    test

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-if ("serviceWorker" in navigator) {
+if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('./serviceWorker.js');
+        navigator.serviceWorker.register('/canteen/serviceWorker.js');
     });
 }
 
