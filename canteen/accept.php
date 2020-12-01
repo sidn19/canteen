8343b3c4deeebf8b0c055127d70f6daabd0a5fb8
ArcTLK <adityanair313@gmail.com>
2020-12-02 03:12:15 +0530

    emails

--- canteen/accept.php
+++ canteen/accept.php
@@ -10,9 +10,38 @@
           $stmt->EXECUTE([$id]);
 
 
+require __DIR__.'/vendor/phpmailer/phpmailer/src/Exception.php';
+require __DIR__.'/vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require __DIR__.'/vendor/phpmailer/phpmailer/src/SMTP.php';
 
-            echo $id;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
+$mail = new PHPMailer();
+$mail->IsSMTP();
+$mail->Mailer = "smtp";
+
+$mail->SMTPDebug  = 1;  
+$mail->SMTPAuth   = TRUE;
+$mail->SMTPSecure = "tls";
+$mail->Port       = 587;
+$mail->Host       = "smtp.gmail.com";
+$mail->Username   = "testcollegecanteen@gmail.com";
+$mail->Password   = "rushishirole9@123456789";
+
+$mail->IsHTML(true);
+$mail->AddAddress($_POST['email'], $_POST['name']);
+$mail->SetFrom("testcollegecanteen@gmail.com", "College Canteen");
+$mail->AddReplyTo("testcollegecanteen@gmail.com", "College Canteen");
+$mail->Subject = "Your order is accepted!";
+$content = "<h1>Thank you for ordering!</h1><p>Food is on the way!.</p>";
+
+$mail->MsgHTML($content); 
+if(!$mail->Send()) {
+    echo 'Error sending email';
+} else {
+  echo $id;
+}
 
 
 ?>
--- canteen/decline.php
+++ canteen/decline.php
@@ -10,9 +10,37 @@
           $stmt->EXECUTE([$id]);
 
 
-
-            echo $id;
-
-
+            require __DIR__.'/vendor/phpmailer/phpmailer/src/Exception.php';
+            require __DIR__.'/vendor/phpmailer/phpmailer/src/PHPMailer.php';
+            require __DIR__.'/vendor/phpmailer/phpmailer/src/SMTP.php';
+            
+            use PHPMailer\PHPMailer\PHPMailer;
+            use PHPMailer\PHPMailer\Exception;
+            
+            $mail = new PHPMailer();
+            $mail->IsSMTP();
+            $mail->Mailer = "smtp";
+            
+            $mail->SMTPDebug  = 1;  
+            $mail->SMTPAuth   = TRUE;
+            $mail->SMTPSecure = "tls";
+            $mail->Port       = 587;
+            $mail->Host       = "smtp.gmail.com";
+            $mail->Username   = "testcollegecanteen@gmail.com";
+            $mail->Password   = "rushishirole9@123456789";
+            
+            $mail->IsHTML(true);
+            $mail->AddAddress($_POST['email'], $_POST['name']);
+            $mail->SetFrom("testcollegecanteen@gmail.com", "College Canteen");
+            $mail->AddReplyTo("testcollegecanteen@gmail.com", "College Canteen");
+            $mail->Subject = "Your order is declined!";
+            $content = "<p>Unfortunately, your order was declined. Maybe try ordering another item.</p>";
+            
+            $mail->MsgHTML($content); 
+            if(!$mail->Send()) {
+                echo 'Error sending email';
+            } else {
+              echo $id;
+            }
 
 ?>
