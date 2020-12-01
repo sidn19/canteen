6572afe315ef6eba1fd0c28e2bc298fcd99cc83e
ArcTLK <adityanair313@gmail.com>
2020-12-02 02:54:24 +0530

    fix

--- canteen/data.php
+++ canteen/data.php
@@ -5,7 +5,15 @@ header('Content-Type: application/json');
 require_once 'connection.php';
 
 //query to get data from the table
-$query = ("SELECT DATE(createdAt) as `date`, SUM(amount) AS amount FROM transactions GROUP BY createdAt ORDER BY createdAt");
+$query = '
+  SELECT createdAt, SUM(amount) AS amount
+    FROM (
+      SELECT DATE(createdAt) AS createdAt, amount
+        FROM transactions
+    ) sub
+  GROUP BY createdAt
+  ORDER BY createdAt
+';
 
 //execute query
 $result = $pdo->query($query);
